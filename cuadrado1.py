092bde3fa66593b136c0707948cd0ce459720d29
wilmarux <wilmar.cabezas@gmail.com>
2021-05-24 21:41:34 -0500

    Mi primer commit

---
+++ cuadrado1.py
@@ -0,0 +1,13 @@
+import turtle
+
+window=turtle.Screen()
+david = turtle.Turtle()
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.forward(50)
+david.left(90)
+david.forward(50)
+david.left(90)
+
+turtle.mainloop()
